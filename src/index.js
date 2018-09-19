c9b6689efe5c4528852c72b5bf34b036b4d1f1df
Georhi Buklaga <buklaga.98@yandex.ru>
2018-09-19 10:11:53 +0300

    second commit, norm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var chek = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
+// var brackets=["(",")","{","}","[","]",'|','|'];
+var lates_open,b;
+  for (let i = 0; i < str.length; i++){
+ for (let j = 0; j < bracketsConfig.length; j++){
+
+   if (str[i]==bracketsConfig[j][0]  ){
+    chek[j][0]++;
+    lates_open=j;
+    b=1;
+    } 
+
+    if (str[i]==bracketsConfig[j][1]){
+      chek[j][1]++;
+      if (lates_open!=j && b==1) {
+        return false;
+      }
+      else{
+        b=2;
+      }
+    }
+    if (chek[j][0]<chek[j][1]) {
+     return false;
+    }
+    if (chek[j][0]<chek[j][1]) {
+      return false;
+     }
+  }
+  }
+   for (let index = 0; index < chek.length-1; index++) {
+     if(chek[index][0]!=chek[index][1])
+        return false;
+   }
+  return true;
 }
+
